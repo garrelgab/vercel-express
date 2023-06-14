4194a979d310610c8757bd80a6dcf8a651904975
Garrel Gabriel <garrelmgabriel@gmail.com>
2023-06-14 19:23:44 +0800

    Test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mysql = require('mysql');
 const app = express()
 const PORT = 4000
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
